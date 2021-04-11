28f0eef150f46e59edb468871babee6af6caa373
curbA <curbaaaa@gmail.com>
2021-04-11 18:53:36 -0300

    Created Basic Header

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { View, StyleSheet } from "react-native";
 import { ThemeProvider } from "react-native-elements";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import IonIcons from "react-native-vector-icons/Ionicons";
 
 // Screens
 import Diary from "./screens/Diary";
 import Foods from "./screens/Foods";
 import Goals from "./screens/Goals";
 
-import IonIcons from "react-native-vector-icons/Ionicons";
+// Components
+import Header from "./components/Header/Header";
 
 const Tab = createBottomTabNavigator();
 
@@ -19,31 +21,30 @@ const theme = {};
 export default function App() {
 	return (
 		<NavigationContainer>
-			<SafeAreaProvider>
-				<ThemeProvider theme={theme}>
-					<Tab.Navigator
-						screenOptions={({ route }) => ({
-							tabBarIcon: ({color, size}) => {
-								switch (route.name) {
-									case "Diary":
-										return <IonIcons name={"fast-food"} size={size} color={color} />;
-										break;
-									case "Foods":
-										return <IonIcons name={"book"} size={size} color={color} />;
-										break;
-									case "Goals":
-										return <IonIcons name={"trophy"} size={size} color={color} />;
-										break;
-								}
+			<ThemeProvider theme={theme}>
+				<Header />
+				<Tab.Navigator
+					screenOptions={({ route }) => ({
+						tabBarIcon: ({ color, size }) => {
+							switch (route.name) {
+								case "Diary":
+									return <IonIcons name={"fast-food"} size={size} color={color} />;
+									break;
+								case "Foods":
+									return <IonIcons name={"book"} size={size} color={color} />;
+									break;
+								case "Goals":
+									return <IonIcons name={"trophy"} size={size} color={color} />;
+									break;
 							}
-						})}
-					>
-						<Tab.Screen name="Diary" component={Diary} />
-						<Tab.Screen name="Foods" component={Foods} />
-						<Tab.Screen name="Goals" component={Goals} />
-					</Tab.Navigator>
-				</ThemeProvider>
-			</SafeAreaProvider>
+						},
+					})}
+				>
+					<Tab.Screen name="Diary" component={Diary} />
+					<Tab.Screen name="Foods" component={Foods} />
+					<Tab.Screen name="Goals" component={Goals} />
+				</Tab.Navigator>
+			</ThemeProvider>
 		</NavigationContainer>
 	);
 }
---
+++ components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+
+const Header = () => {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>OpenFit</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		marginTop: StatusBar.currentHeight,
+		padding: 14,
+		backgroundColor: "cornflowerblue",
+	},
+	title: {
+		color: "white",
+    fontSize:16,
+	},
+});
+
+export default Header;
