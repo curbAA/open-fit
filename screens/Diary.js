dc2beb9db747a62f055a0ddddc57308c70be3def
curbA <curbaaaa@gmail.com>
2021-04-11 19:12:53 -0300

    Implemented Basic List

--- screens/Diary.js
+++ screens/Diary.js
@@ -1,10 +1,47 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { ListItem } from "react-native-elements";
+
+const list = [
+	{
+		name: "Egg",
+		amount: 2,
+		unit: "units",
+		kcal: 75
+	},
+	{
+		name: "Cheese",
+		amount: 143,
+		unit: "g",
+		kcal: 2
+	},
+	{
+		name: "Milk",
+		amount: 200,
+		unit: "ml",
+		kcal: 0.7
+	},
+	{
+		name: "Steak",
+		amount: 245,
+		unit: "g",
+		kcal: 1.7
+	},
+];
 
 const Diary = () => {
 	return (
 		<View style={styles.container}>
-			<Text>Diary</Text>
+			{
+				list.map((l,i) => (
+					<ListItem key={i} bottomDivider>
+						<ListItem.Content>
+							<ListItem.Title>{l.name}</ListItem.Title>
+							<ListItem.Subtitle>{l.amount + " " + l.unit}</ListItem.Subtitle>
+						</ListItem.Content>
+					</ListItem>
+				))
+			}
 		</View>
 	);
 };
@@ -13,8 +50,5 @@ export default Diary;
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
 	},
 });
