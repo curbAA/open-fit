ae1ae2de0c551dd9dbc2b6d53768ad68fd446580
curbA <curbaaaa@gmail.com>
2021-04-12 04:30:02 -0300

    Moved Diary Data to State

--- screens/Diary/Diary.js
+++ screens/Diary/Diary.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, ScrollView } from "react-native";
 
 // Components
@@ -6,57 +6,57 @@ import MacroHeader from "./components/MacroHeader/MacroHeader";
 import Header from "./components/Header/Header";
 import ListItem from "./components/ListItem/ListItem";
 
-const foodList = [
-	{
-		name: "Egg",
-		amount: 2,
-		unit: "units",
-		kcal: 75,
-	},
-	{
-		name: "Cheese",
-		amount: 143,
-		unit: "g",
-		kcal: 2,
-	},
-	{
-		name: "Milk",
-		amount: 200,
-		unit: "ml",
-		kcal: 0.7,
-	},
-	{
-		name: "Steak",
-		amount: 245,
-		unit: "g",
-		kcal: 1.7,
-	},
-];
-
-const exerciseList = [
-	{
-		name: "Burpees",
-		minutes: 20,
-		kcal: 251,
-	},
-	{
-		name: "Running",
-		minutes: 20,
-		kcal: 217,
-	},
-	{
-		name: "Weightlifting",
-		minutes: 20,
-		kcal: 195,
-	},
-	{
-		name: "Weightlifting",
-		minutes: 20,
-		kcal: 195,
-	},
-];
 
 const Diary = () => {
+	const [foodList, setFoodList] = useState([
+		{
+			name: "Egg",
+			amount: 2,
+			unit: "units",
+			kcal: 75,
+		},
+		{
+			name: "Cheese",
+			amount: 143,
+			unit: "g",
+			kcal: 2,
+		},
+		{
+			name: "Milk",
+			amount: 200,
+			unit: "ml",
+			kcal: 0.7,
+		},
+		{
+			name: "Steak",
+			amount: 245,
+			unit: "g",
+			kcal: 1.7,
+		},
+	]);
+	const [exerciseList, setExerciseList] = useState([
+		{
+			name: "Burpees",
+			minutes: 20,
+			kcal: 251,
+		},
+		{
+			name: "Running",
+			minutes: 20,
+			kcal: 217,
+		},
+		{
+			name: "Weightlifting",
+			minutes: 20,
+			kcal: 195,
+		},
+		{
+			name: "Weightlifting",
+			minutes: 20,
+			kcal: 195,
+		},
+	]);
+
 	return (
 		<View style={styles.container}>
 			<MacroHeader />
