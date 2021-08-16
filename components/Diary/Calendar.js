aa323ffaafd26b803a8c7e5de07e2147dc2d3988
curbA <curbaaaa@gmail.com>
2021-08-16 20:01:56 -0300

    Implemented TouchableOpacity in Calendar

--- components/Diary/Calendar.js
+++ components/Diary/Calendar.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-elements";
 import CalendarPicker from "react-native-calendar-picker";
 import moment from "moment";
+
+// Components
+import TouchableOpacity from "openfit/components/TouchableOpacity";
 
 const Calendar = () => {
 	const [selectedDate, setSelectedDate] = useState(moment());
 	const DATE_FORMAT = "MMMM Do YYYY";
-	const currentDate = moment();
 
 	const handleDateChange = (date) => {
 		setSelectedDate(date);
@@ -15,17 +18,39 @@ const Calendar = () => {
 	const [displayCalendar, setDisplayCalendar] = useState(false);
 	const toggleCalendar = () => setDisplayCalendar(!displayCalendar);
 
+	const handleOk = () => {
+		toggleCalendar();
+	};
+
+	const handleCancel = () => {
+		toggleCalendar();
+	};
+
 	return (
 		<View>
-			<Pressable onPress={toggleCalendar}>
+			<TouchableOpacity onPress={toggleCalendar}>
 				<Text style={styles.selectedDate}>
 					{selectedDate.format(DATE_FORMAT) == moment().format(DATE_FORMAT)
 						? selectedDate.format(DATE_FORMAT) + " (Today)"
 						: selectedDate.format(DATE_FORMAT)}
 				</Text>
-			</Pressable>
+			</TouchableOpacity>
 			{displayCalendar ? (
-				<CalendarPicker selectedStartDate={selectedDate.format()} onDateChange={handleDateChange} />
+				<>
+					<CalendarPicker
+						selectedStartDate={selectedDate.format()}
+						onDateChange={handleDateChange}
+					/>
+					<View style={styles.buttonContainer}>
+						<Button onPress={handleOk} containerStyle={styles.button} title="Ok" />
+						<Button
+							onPress={handleCancel}
+							containerStyle={styles.button}
+							type="outline"
+							title="Cancel"
+						/>
+					</View>
+				</>
 			) : (
 				<></>
 			)}
@@ -41,5 +66,12 @@ const styles = StyleSheet.create({
 		padding: 10,
 		borderColor: "#cecece",
 		borderWidth: 1,
+	},
+	buttonContainer: {
+		flexDirection: "row-reverse",
+	},
+	button: {
+		width: 100,
+		margin: 10,
 	},
 });
